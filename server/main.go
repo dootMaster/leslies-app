797e8f72c405b98210eb7c2b87974a7697bef277
Leslie Ngo <ngo.leslie@gmail.com>
2023-07-24 18:40:38 -0700

    second commit!

--- server/main.go
+++ server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "database/sql"
+	"database/sql"
 	"fmt"
 	_ "log"
 	_ "net/http"
@@ -9,6 +9,28 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "ilovebeagranno"
+	dbname   = "lesliesapp"
+)
+
 func main() {
-	fmt.Println("Hello, World!")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
